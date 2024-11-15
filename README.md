57001964daafb6843b2c836237ca267a177c6631
Erik Dittrich <erik.dittrich@gmail.com>
2024-11-15 18:41:09 +0100

    Initial

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # spike-webscraping
+
+https://www.crummy.com/software/BeautifulSoup/bs4/doc/  
+https://www.geeksforgeeks.org/how-to-use-lxml-with-beautifulsoup-in-python/  
+https://blog.apify.com/beautifulsoup-find-by-class/  
+  
+https://arztsuchehessen.de/
--- scraper.py
+++ scraper.py
@@ -1,11 +1,7 @@
 from bs4 import BeautifulSoup
-from lxml import etree
 
-with open('data/in/test.html', 'r', encoding='utf-8') as file:
+with open('data/in/Arztsuche1.mhtml', 'r', encoding='utf-8') as file:
     html_content = file.read()
 soup = BeautifulSoup(html_content, 'lxml')
 title = soup.title.string
 print(f"Title of the HTML document: {title}")
-paragraphs = soup.find_all('p')
-for idx, p in enumerate(paragraphs, start=1):
-    print(f"Paragraph {idx}: {p.text.strip()}")
