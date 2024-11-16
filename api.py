d9410989e811fa758f615fc5285acf30c822a118
Erik Dittrich <erik.dittrich@gmail.com>
2024-11-16 13:18:01 +0100

    Initial

--- api.py
+++ api.py
@@ -6,4 +6,4 @@ with open('data/in/Arztsuche.json', encoding='utf-8') as inputfile:
     data = json.loads(inputfile.read())
     df = pd.json_normalize(data)
 
-df.to_csv('data/out/Doctors_api.csv', encoding='utf-8', index=False, quoting=csv.QUOTE_ALL, sep=';')
+df.to_csv('data/out/Doctors_api.csv', encoding='utf-8', index=False, quoting=csv.QUOTE_ALL, quotechar='"', sep=';')
